69ede2fdd2ff5fea7406c47b61c037b4808c02a9
Blame Reiker <reaperofssa@gmail.com>
2025-02-12 11:12:43 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ echo "Installing required libraries for Puppeteer..."
 apt-get install -y libnss3 libatk-bridge2.0-0 libxcomposite1 libxrandr2 libgbm1 libasound2 libpangocairo-1.0-0 libcups2
 
 echo "Installing Puppeteer Chrome..."
-npx puppeteer install
+npx puppeteer browsers install chrome
